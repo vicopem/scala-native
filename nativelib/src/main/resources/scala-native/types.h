8ec81379ac56c5f311c35908d4cc482a28ed13ee
Victor Cochard <victor.cochard@epfl.ch>
2020-11-19 16:33:45 +0100

    Revert "Move native code into posixlib and clib respectively (#1885)"
    This reverts commit 39ef208f3d1b9195379da4b85e70b0dcf952c42b.

--- nativelib/src/main/resources/scala-native/types.h
+++ nativelib/src/main/resources/scala-native/types.h
@@ -1,8 +1,6 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-// types used in pwd.c and sys/stat.c
-
 typedef unsigned long scalanative_dev_t;
 typedef unsigned int scalanative_mode_t;
 typedef unsigned long long scalanative_ino_t;
