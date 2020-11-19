6312486ab46ec37dcc1fcb34bc0724f738954a49
Victor Cochard <victor.cochard@epfl.ch>
2020-11-19 16:33:45 +0100

    Revert "Port scala.scalanative tests to JUnit (#1989)"
    This reverts commit c3bf2daf1f9e58a8501ff4ecc11488d3c6ec4a55.

--- unit-tests/src/test/scala/utils/ThrowsHelper.scala
+++ unit-tests/src/test/scala/utils/ThrowsHelper.scala
@@ -2,9 +2,6 @@ package scala.scalanative.junit.utils
 
 import AssertThrows._
 
-// Calls to this should probably be changed to expectThrows.
-// This was added as it was all over the place in the pre
-// JUnit code.
 object ThrowsHelper {
   def assertThrowsAnd[T <: Throwable, U](
       expectedThrowable: Class[T],
